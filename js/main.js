39ada896d251b2d850f5385d9e1f77679fe7f2fc
ashLuchowa <ashwinluchowa@gmail.com>
2024-05-06 15:14:43 +0800

    Implement gameBoard and win condition functions

--- js/main.js
+++ js/main.js
@@ -1,28 +1,99 @@
-// create user factory function
-function createUser(userName, userMarker) {
+// Create user factory function
+function createUser(userMarker) {
     let userScore = 0;
     const getScore = () => userScore;
     const giveScore = () => userScore++;
-    return {userName, userMarker, giveScore, getScore};
+    return {userMarker, giveScore, getScore};
 }
 
-const player1 = createUser('Ash', 'X');
-const player2 = createUser('Krishty', 'O');
-let playerTurn = player1;
+const playerX = createUser('X');
+const playerO = createUser('O');
+let playerTurn = playerX;
 
 
-// displayControllers
-const displayControllers = ((player1Score, player2Score) => {
+// DisplayControllers
+const displayControllers = ((playerXScore, playerOScore) => {
     let currentRound = 0;
 
-    player1Score = document.querySelector('#player1-score');
-    player2Score = document.querySelector('#player2-score');
+    playerXScore = document.querySelector('#playerx-score');
+    playerOScore = document.querySelector('#playero-score');
     gameRound = document.querySelector('#round-num');
 
-    // update player score
-    player1Score.textContent = player1.getScore();
-    player2Score.textContent = player2.getScore();
+    // Update player score
+    playerXScore.textContent = playerX.getScore();
+    playerOScore.textContent = playerO.getScore();
 
-    // update round
+    // Update round
     gameRound.textContent = currentRound;
 })();
+
+
+// Gameboard object
+const board = [['','',''],['','',''],['','','']];
+
+
+// Display markers on each squares
+const squareHandling = (function() {
+    const squares = document.querySelectorAll('.square');
+    
+    squares.forEach((square) => {
+        square.addEventListener('click', (e) => {
+            if(!e.target.textContent) {
+                if(playerTurn === playerX) {
+                    square.textContent = playerX.userMarker;
+                    playerTurn = playerO;
+                } else {
+                    square.textContent = playerO.userMarker;
+                    playerTurn = playerX;
+                }
+                // Update board array
+                updateBoard(e.target.id, square, board, playerTurn);
+            }
+            console.log(board);
+        });
+    });
+})();
+
+
+// Update the board array
+const updateBoard = function(squareId, square, board) {
+    const squareItem = {
+        'square1': [0,0],
+        'square2': [0,1],
+        'square3': [0,2],
+        'square4': [1,0],
+        'square5': [1,1],
+        'square6': [1,2],
+        'square7': [2,0],
+        'square8': [2,1],
+        'square9': [2,2],
+    }
+    
+    // Save each click into the board array
+    const boardSlots = squareItem[squareId];
+    [row, column] = boardSlots;
+    if(boardSlots) {
+        board[row][column] = square.textContent;
+    }
+
+    winGame();
+};
+
+
+// Win conditions
+const winGame = function() {
+    function winCondition(playerId) {
+        return board[0][0] === playerId && board[0][1] === playerId && board[0][2] === playerId ||
+               board[1][0] === playerId && board[1][1] === playerId && board[1][2] === playerId ||
+               board[2][0] === playerId && board[2][1] === playerId && board[2][2] === playerId ||
+               board[0][0] === playerId && board[1][0] === playerId && board[2][0] === playerId ||
+               board[0][1] === playerId && board[1][1] === playerId && board[2][1] === playerId ||
+               board[0][2] === playerId && board[1][2] === playerId && board[2][2] === playerId ||
+               board[0][0] === playerId && board[1][1] === playerId && board[2][2] === playerId ||
+               board[2][0] === playerId && board[1][1] === playerId && board[0][2] === playerId;
+    };
+    
+    if(winCondition(playerX.userMarker)) {
+        console.log(`${playerX.userMarker} wins!`);
+    }
+}
